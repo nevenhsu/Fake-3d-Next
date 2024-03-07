4a51124cb05fb6968db210f3d0009fa478446728
Neven <space.man0318@gmail.com>
2024-03-07 20:00:51 +1100

    update images

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Sketch from '@/components/Sketch'
 import classes from './page.module.css'
 
-const list = ['ai', 'cloud', 'design', 'dev', 'ether']
+const list = ['ai', 'cloud', 'design', 'dev', 'eth']
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
             key={img}
             uid={img}
             imageUrl={`/images/${img}.png`}
-            depthUrl={`/images/${img}-depth.jpg`}
+            depthUrl={`/images/${img}-d.jpg`}
           />
         </div>
       ))}
